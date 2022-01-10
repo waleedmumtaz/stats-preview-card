84ae142cb186c3d32d7f6caa8b13ba2df0da8bb4
Waleed <waleedthedev@gmail.com>
2022-01-11 00:17:49 +0500

    Clean up and setup according to style guides

---
+++ src/lib/main-content.svelte
@@ -0,0 +1,14 @@
+<main>
+  <!--
+  
+  Get insights that help your business grow.
+
+  Discover the benefits of data analytics and make better decisions regarding revenue, customer 
+  experience, and overall efficiency.
+
+  10k+ companies
+  314 templates
+  12m+ queries
+  
+  -->
+</main>
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,11 +1,29 @@
 const config = {
-  content: ["./src/**/*.{html,js,svelte,ts}"],
+  content: ['./src/**/*.{html,js,svelte,ts}'],
 
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        cstm: {
+          primary: {
+            'dark-blue': 'hsl(233, 47%, 7%)',
+            'dark-desaturated-blue': 'hsl(244, 38%, 16%)',
+            'soft-violet': 'hsl(277, 64%, 61%)',
+          },
+          neutral: {
+            'transparent-white-paragraph': 'hsla(0, 0%, 100%, 0.75)',
+            'transparent-white-stat-headings': 'hsla(0, 0%, 100%, 0.6)',
+          },
+        },
+      },
+      fontFamily: {
+        inter: ['Inter', 'sans-serif'],
+        'lexend-deca': ['Lexend Deca', 'sans-serif'],
+      },
+    },
   },
 
   plugins: [],
-};
+}
 
-module.exports = config;
+module.exports = config
